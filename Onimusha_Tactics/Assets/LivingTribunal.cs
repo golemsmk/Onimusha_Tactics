bfe6b84b26ec4c07ad2105e8efc1ed6f0d63490b
Thuong Long <golemsmk1@gmail.com>
2018-11-01 22:31:20 +0700

    Test Map Creating

---
+++ Onimusha_Tactics/Assets/LivingTribunal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingTribunal : MonoBehaviour {
+
+    public GameObject cubePrefab, riverPrefab;
+
+    public int height = 10;
+    public int width = 8;
+    
+    // Use this for initialization
+	void Start () {
+        GameObject tempGameObject;
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                tempGameObject = Instantiate(cubePrefab);
+                tempGameObject.transform.position = new Vector3(1 * i, 0, 1 * j);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
